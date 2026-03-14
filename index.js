5d60e755f1896a311ce8af3c766fcc252f481a56
SheillaO <sheilla.alusa@strathmore.edu>
2026-03-14 22:56:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ GLP-1 Companion - Conversion Tool for Ozempic/Mounjaro Users
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const inputEl = document.getElementById("input-el");
+const lengthEl = document.getElementById("length-el");
+const massEl = document.getElementById("mass-el");
+const volumeEl = document.getElementById("volume-el");
+const convertBtn = document.getElementById("convert-btn");
